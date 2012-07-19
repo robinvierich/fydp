14d511606cdf6dbc85380ea346dcbe6e501fc6be
Zack MacLennan <zrmac22@gmail.com>
2012-07-19 18:36:08 -0400

    Added Free Form Mode plug-in

---
+++ regis/RegisFreeFormPlugin/FreeFormControl.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel.Composition;
+using Regis.Plugins;
+
+
+namespace RegisFreeFormPlugin
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+
+    [Export(typeof(IPlugin))]
+    public partial class FreeFormControl : UserControl, IPlugin
+    {
+        public FreeFormControl()
+        {
+            InitializeComponent();
+        }
+
+        #region IPlugin
+
+        public void Load()
+        {
+            
+        }
+
+        public FrameworkElement GetVisualContent()
+        {
+            return this;
+        }
+
+        public string PluginName
+        {
+            get { return "FreeFormPlugin"; }
+        }
+
+        public string FriendlyPluginName
+        {
+            get { return "FreeFormMode"; }
+        }
+
+        public NoteDetectionAlgorithm Algorithm
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        #endregion
+    }
+}
