e0bacf627759aa00d596ffc810bbde332cf4db2c
Robin Vierich <robinvierich@gmail.com>
2013-03-15 18:58:24 -0400

    * Added TestModule project as example of base module that accepts a stream of notes.

---
+++ regis/TestModule/TestControl.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel.Composition;
+using Regis.Plugins.Interfaces;
+
+namespace TestModule
+{
+    /// <summary>
+    /// Interaction logic for TestControl.xaml
+    /// </summary>
+    public partial class TestControl : UserControl, IPartImportsSatisfiedNotification, IPlugin
+    {
+        public TestControl() {
+            InitializeComponent();
+        }
+
+        [Import]
+        public TestViewModel ViewModel {
+            get;
+            set;
+        }
+
+        public void OnImportsSatisfied() {
+            DataContext = ViewModel;
+        }
+
+        public void Load() {
+        }
+
+        public FrameworkElement GetVisualContent() {
+            return this;
+        }
+
+        public string PluginName {
+            get {
+                return "TestPlugin";
+            }
+        }
+
+        public string FriendlyPluginName {
+            get {
+                return "Test Plugin";
+            }
+        }
+    }
+}
---
+++ regis/TestModule/TestViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Regis.Plugins.Interfaces;
+using System.ComponentModel.Composition;
+using Regis.Plugins.Models;
+using System.Collections.ObjectModel;
+using Regis.Base.ViewModels;
+using System.Windows.Threading;
+
+
+
+namespace TestModule
+{
+    [Export]
+    public class TestViewModel : BaseViewModel, IPartImportsSatisfiedNotification
+    {
+        [Import]
+        private INoteDetectionSource _ns;
+
+        private DispatcherTimer timer;
+
+        public TestViewModel() {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Tick += new EventHandler(timer_Tick);
+            Notes = new ObservableCollection<Note>();
+        }
+
+        void timer_Tick(object sender, EventArgs e) {
+            Note[] notes = _ns.GetNotes();
+            foreach (Note note in notes) {
+                this.Notes.Add(note);
+            }
+        }
+
+        private ObservableCollection<Note> _Notes;
+        public ObservableCollection<Note> Notes {
+            get {
+                return _Notes;
+            }
+            set {
+                _Notes = value;
+                NotifyPropertyChanged(new System.ComponentModel.PropertyChangedEventArgs("Notes"));
+            }
+        }
+
+
+        public void OnImportsSatisfied() {
+            timer.Start();
+        }
+    }
+}
