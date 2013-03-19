4c5ca86489b97e48861cfed6d3b5615ad33b6bf9
Robin Vierich <robinvierich@gmail.com>
2013-03-19 15:39:48 -0400

    * Added Achievement Service

---
+++ regis/RegisPlayAlongPlugin/PlayedFirstSongAchievement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Regis.Plugins.Models;
+
+namespace RegisPlayAlongPlugin
+{
+    public class PlayedFirstSongAchievement : Achievement
+    {
+
+        static int _Count;
+
+        static PlayedFirstSongAchievement() {
+            _Count++;
+        }
+
+        public override string String {
+            get { return "Played first song!"; }
+        }
+
+        public override int Count {
+            get { return _Count; }
+        }
+    }
+}
---
+++ regis/regis/Services/Impl/AchievementService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Regis.Plugins.Interfaces;
+using Regis.Plugins.Models;
+using System.ComponentModel.Composition;
+
+namespace Regis.Services.Impl
+{
+    [Export(typeof(IAchievementService))]
+    public class AchievementService: IAchievementService
+    {
+        private List<Achievement> _achievements;
+
+        AchievementService() {
+            _achievements = new List<Achievement>();
+        }
+
+        public void SetAchievement(Achievement achievement) {
+            _achievements.Add(achievement);
+            Raise_NewAchievement(achievement);
+        }
+
+        public event EventHandler<NewAchievementEventArgs> NewAchievement;
+
+        private void Raise_NewAchievement(Achievement achievement) {
+            EventHandler<NewAchievementEventArgs> h = NewAchievement;
+            if (h == null) return;
+
+            h(this, new NewAchievementEventArgs(achievement));
+        }
+    }
+}
