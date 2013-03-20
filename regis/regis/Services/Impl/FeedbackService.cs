2b599439e2b3c81ff8e289814466515172216d31
Robin Vierich <robinvierich@gmail.com>
2013-03-19 20:07:54 -0400

    Added basic timing feedback

--- regis/regis/Services/Impl/FeedbackService.cs
+++ regis/regis/Services/Impl/FeedbackService.cs
@@ -14,32 +14,68 @@ namespace Regis.Services.Impl
 
         double maxTimeDiffForMatch = 0.1; // s
 
-        private Dictionary<Note, Note> GetMatches(IList<Note> playedNotes, IList<Note> goalNotes){
-            Dictionary<Note, Note> matches = new Dictionary<Note, Note>();
+        private Dictionary<Note, List<Note>> GetMatches(IList<Note> playedNotes, IList<Note> goalNotes){
+            Dictionary<Note, List<Note>> matches = new Dictionary<Note, List<Note>>();
 
             // look for notes where time difference is within a threshold (maxTimeDiffForMatch)
             foreach (Note goalNote in goalNotes) {
-                IList<Note> matchedNotes = playedNotes.Where(playedNote => {
+                List<Note> matchedNotes = playedNotes.Where(playedNote => {
                     return Math.Abs((playedNote.startTime - goalNote.startTime).TotalSeconds) < maxTimeDiffForMatch;
                 }).ToList();
 
-                if (matchedNotes.Count > 0) {
-                    int i = 0;
-                }
+                matches[goalNote] = matchedNotes;
             }
 
             return matches;
         }
 
-        public Feedback GetFeedback(IList<Note> playedNotes, IList<Note> goalNotes) {
+
+        private Note FindClosestNote(Note goalNote, IList<Note> playedNotes) {
+            Note closestNote = playedNotes[0];
+            double minDiff = Math.Abs((closestNote.startTime - goalNote.startTime).TotalSeconds);
+            foreach (Note playedNote in playedNotes) {
+                double diff = Math.Abs((closestNote.startTime - goalNote.startTime).TotalSeconds);
+                if (diff < minDiff) {
+                    minDiff = diff;
+                    closestNote = playedNote;
+                }
+            }
+            return closestNote;
+        }
+
+        private Feedback GetTimingFeedback(Note goalNote, IList<Note> playedNotes) {
+            Note closestNote = FindClosestNote(goalNote, playedNotes);
+
+            double diff = (closestNote.startTime - goalNote.startTime).TotalSeconds;
+            double absDiff = Math.Abs(diff);
+
+            string slowOrFastStr = diff > 0 ? "slow" : "fast"; // user is slow if difference is positive (played after goal note)
+
+            if (absDiff < 0.03) {
+                return new Feedback() { FeedbackString = "Good timing!" };
+            } else if (absDiff < 0.05) {
+                return new Feedback() { FeedbackString = String.Format("You were just a little bit {0}", slowOrFastStr) };
+            } else {
+                return new Feedback() { FeedbackString = String.Format("You were {0}", slowOrFastStr) };
+            }
+        }
+
+        public List<Feedback> GetFeedback(IList<Note> allPlayedNotes, IList<Note> goalNotes) {
             string feedbackString = string.Empty;
 
-            Dictionary<Note, Note> matches = GetMatches(playedNotes, goalNotes);
-            if (matches.Count > 0) {
-                int i = 0;
+            List<Feedback> feedback = new List<Feedback>();
+
+            Dictionary<Note, List<Note>> matches = GetMatches(allPlayedNotes, goalNotes);
+
+            // loop through each match
+            foreach (KeyValuePair<Note, List<Note>> match in matches.Where(match => match.Value.Count > 0)) {
+                Note goalNote = match.Key;
+                List<Note> playedNotes = match.Value;
+
+                feedback.Add(GetTimingFeedback(goalNote, playedNotes));
             }
 
-            return new Feedback() { FeedbackString = feedbackString };
+            return feedback;
         }
     }
 }
